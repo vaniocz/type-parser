0133b3262ef4048839ef9810703c0c1313b1dc10
Marek Štípek <mario.dweller@seznam.cz>
2016-03-12 14:18:36 +0100

    Modified caching key so it handles invalidation when parent class is modified

--- tests/CachingParserTest.php
+++ tests/CachingParserTest.php
@@ -4,6 +4,7 @@ namespace Vanio\TypeParser\Tests;
 use Doctrine\Common\Cache\ArrayCache;
 use Vanio\TypeParser\CachingParser;
 use Vanio\TypeParser\SimpleType;
+use Vanio\TypeParser\Tests\Fixtures\Bar;
 use Vanio\TypeParser\Tests\Fixtures\Foo;
 use Vanio\TypeParser\Type;
 use Vanio\TypeParser\TypeParser;
@@ -23,17 +24,17 @@ class CachingParserTest extends \PHPUnit_Framework_TestCase
     {
         $this->types = [new SimpleType(Type::STRING)];
         $this->typeParser = $this->getMockWithoutInvokingTheOriginalConstructor(TypeParser::class);
-        $this->typeParser->expects($this->once())
-            ->method('parsePropertyTypes')
-            ->with(Foo::class)
-            ->willReturn($this->types);
         $this->cache = $this->getMockBuilder(ArrayCache::class)->enableProxyingToOriginalMethods()->getMock();
     }
 
     function test_parsing_property_types()
     {
+        $this->typeParser->expects($this->once())
+            ->method('parsePropertyTypes')
+            ->with(Foo::class)
+            ->willReturn($this->types);
         $cacheIdPattern = sprintf(
-            '~%s\[.*\Foo\.php\]\[\d+\]\[%s]$~',
+            '~%s\[.*\Foo\.php\]\[\d+]\[%s]$~',
             preg_quote(CachingParser::class),
             preg_quote(Foo::class)
         );
@@ -51,8 +52,37 @@ class CachingParserTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(1, $this->cache->getStats()['hits']);
     }
 
+    function test_parsing_child_class_property_types()
+    {
+        $this->typeParser->expects($this->once())
+            ->method('parsePropertyTypes')
+            ->with(Bar::class)
+            ->willReturn($this->types);
+        $cacheIdPattern = sprintf(
+            '~%s\[.*\Bar\.php\]\[\d+,\d+\]\[%s]$~',
+            preg_quote(CachingParser::class),
+            preg_quote(Bar::class)
+        );
+        $this->cache->expects($this->any())
+            ->method('fetch')
+            ->with($this->matchesRegularExpression($cacheIdPattern));
+
+        $cachingParser = new CachingParser($this->typeParser, $this->cache);
+        $this->assertEquals($this->types, $cachingParser->parsePropertyTypes(Bar::class));
+
+        $cachingParser = new CachingParser($this->typeParser, $this->cache);
+        $this->assertEquals($this->types, $cachingParser->parsePropertyTypes(Bar::class));
+        $this->assertEquals($this->types, $cachingParser->parsePropertyTypes(Bar::class));
+
+        $this->assertSame(1, $this->cache->getStats()['hits']);
+    }
+
     function test_parsing_property_types_without_cache_invalidation()
     {
+        $this->typeParser->expects($this->once())
+            ->method('parsePropertyTypes')
+            ->with(Foo::class)
+            ->willReturn($this->types);
         $this->cache->expects($this->any())
             ->method('fetch')
             ->with(sprintf('%s[%s]', CachingParser::class, Foo::class));
